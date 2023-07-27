57fd9ca3ab19b8b97e53bdd5bf3a8a75e620ddf6
Alex <125411159+110614Yei@users.noreply.github.com>
2023-07-27 13:42:42 -0600

    ARCHIVOS RAMA ANTERIOR

---
+++ SIMEXPRO Produccion/src/app/configs/navigationConfig.js
@@ -0,0 +1,401 @@
+import i18next from 'i18next';
+import '@material-ui/icons';
+import ar from './navigation-i18n/ar';
+import en from './navigation-i18n/en';
+import tr from './navigation-i18n/tr';
+
+i18next.addResourceBundle('en', 'navigation', en);
+i18next.addResourceBundle('tr', 'navigation', tr);
+i18next.addResourceBundle('ar', 'navigation', ar);
+
+const navigationConfig = [
+  {
+    id: 'Inicio',
+    title: 'Inicio',
+    type: 'group',
+    icon: 'heroicons-outline:home',
+    translate: 'INICIO',
+    children: [
+      {
+        id: 'Inicio.Inicio',
+        title: 'Inicio',
+        type: 'item',
+        icon: 'heroicons-outline:clipboard-check',
+        url: '/dashboards/analytics',
+      },
+    ],
+  },
+  {
+    id: 'Seguridad',
+    title: 'Seguridad',
+    type: 'group',
+    icon: 'material-outline:content_cut',
+    translate: 'SEGURIDAD',
+    children: [
+      {
+        id: 'apps.ecommerce',
+        title: 'ECommerce',
+        type: 'collapse',
+        icon: 'material-outline:content_cut',
+        translate: 'Acceso',
+        children: [
+          {
+            id: 'Seguridad.Usuarios',
+            title: 'Usuarios',
+            type: 'item',
+            icon: 'material-outline:hail',
+            url: '/Usuarios/Index',
+          },
+          {
+            id: 'Seguridad.Roles',
+            title: 'Roles',
+            type: 'item',
+            icon: 'material-outline:manage_search',
+            url: '/Roles/RolesIndex',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'Generales',
+    title: 'Generales',
+    type: 'group',
+    icon: 'heroicons-outline:home',
+    translate: 'GENERALES',
+    children: [
+      {
+        id: 'Generales.Generales',
+        title: 'ECommerce',
+        type: 'collapse',
+        icon: 'heroicons-outline:bookmark-alt',
+        translate: 'Generales',
+        children: [
+          {
+            id: 'Generales.formas_envio',
+            title: 'Formas de envío',
+            type: 'item',
+            icon: 'material-outline:taxi_alert',
+            url: '/FormaDeEnvio/Index',
+          },
+          {
+            id: 'Generales.monedas',
+            title: 'Monedas',
+            type: 'item',
+            icon: 'material-outline:attach_money',
+            url: 'Monedas/Index',
+          },
+          {
+            id: 'Personas.oficinas',
+            title: 'Oficinas',
+            type: 'item',
+            icon: 'material-outline:computer',
+            url: 'Oficinas/Index',
+          },
+          {
+            id: 'Personas.oficios_profesiones',
+            title: 'Oficios y Profesiones',
+            type: 'item',
+            icon: 'heroicons-outline:academic-cap',
+            url: 'OficiosProfesiones/Index',
+          },
+        ],
+      },
+      {
+        id: 'general.ubicaciones',
+        title: 'Ubicaciones',
+        type: 'collapse',
+        icon: 'material-outline:public',
+        translate: 'Ubicaciones',
+        children: [
+          {
+            id: 'Ubicaciones.paises',
+            title: 'Países',
+            type: 'item',
+            icon: 'material-outline:map',
+            url: 'Paises/Index',
+          },
+          {
+            id: 'Ubicaciones.provincias',
+            title: 'Provincias',
+            type: 'item',
+            icon: 'material-outline:house',
+            url: 'Provincias/Index',
+          },
+          {
+            id: 'Ubicaciones.ciudades',
+            title: 'Ciudades',
+            type: 'item',
+            icon: 'material-outline:business',
+            url: 'Ciudades/Index',
+          },
+          {
+            id: 'Ubicaciones.aldeas',
+            title: 'Aldeas',
+            type: 'item',
+            icon: 'material-outline:cabin',
+            url: '/Aldea/Index',
+          },
+          {
+            id: 'Ubicaciones.colonias',
+            title: 'Colonias',
+            type: 'item',
+            icon: 'material-outline:holiday_village',
+            url: '/Colonias/Index',
+          },
+        ],
+      },
+      {
+        id: 'general.Personas',
+        title: 'Personas',
+        type: 'collapse',
+        icon: 'material-outline:emoji_people',
+        translate: 'Personas',
+        children: [
+          {
+            id: 'Personas.cargos',
+            title: 'Cargos',
+            type: 'item',
+            icon: 'material-outline:cleaning_services',
+            url: 'Cargos/Index',
+          },
+
+          {
+            id: 'Personas.empleados',
+            title: 'Empleados ',
+            type: 'item',
+            icon: 'material-outline:groups',
+            url: 'Empleados/Index',
+          },
+          {
+            id: 'Personas.proveedores',
+            title: 'Proveedores ',
+            type: 'item',
+            icon: 'heroicons-solid:user',
+            url: '/Proveedores/Index',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'Produccion',
+    title: 'Produccion',
+    type: 'group',
+    icon: 'material-outline:content_cut',
+    translate: 'PRODUCCIÓN',
+    children: [
+      {
+        id: 'apps.ecommerce',
+        title: 'ECommerce',
+        type: 'collapse',
+        icon: 'material-outline:content_cut',
+        translate: 'Producción',
+        children: [
+          {
+            id: 'Produccion.AsignacionesModulo',
+            title: 'Planificación',
+            type: 'item',
+            icon: 'material-outline:hail',
+            url: '/Planificacion/Index',
+          },
+          {
+            id: 'Produccion.Inspecciones',
+            title: 'Inspecciones',
+            type: 'item',
+            icon: 'material-outline:manage_search',
+            url: '/Inspecciones/Index',
+          },
+          {
+            id: 'Produccion.Modulos',
+            title: 'Módulos',
+            type: 'item',
+            icon: 'material-outline:iron',
+            url: '/Modulo/Index',
+          },
+          {
+            id: 'Produccion.OrdenProcesos',
+            title: 'Orden de Procesos',
+            type: 'item',
+            icon: 'material-outline:insights',
+            url: '/OrdenProcesos/Index',
+          },
+          {
+            id: 'Produccion.OrdenCompra',
+            title: 'Órden de Compra',
+            type: 'item',
+            icon: 'material-outline:library_books',
+            url: '/OrdenCompra/Index',
+          },
+          {
+            id: 'Produccion.OrdenPedido',
+            title: 'Órden de Pedido',
+            type: 'item',
+            icon: 'material-outline:library_books',
+            url: '/OrdenPedido/Index',
+          },
+          {
+            id: 'Produccion.PedidosProduccion',
+            title: 'Pedidos de Producción',
+            type: 'item',
+            icon: 'material-outline:local_mall',
+            url: 'PedidosProduccion/Index',
+          },
+          {
+            id: 'Produccion.Procesos',
+            title: 'Procesos',
+            type: 'item',
+            icon: 'material-outline:mediation',
+            url: '/Procesos/Index',
+          },
+          {
+            id: 'Produccion.RevisionCalidad',
+            title: 'Revisión de Calidad',
+            type: 'item',
+            icon: 'material-outline:pageview',
+            url: '/RevisionCalidad/Index',
+          },
+  
+        ],
+      },
+      {
+        id: 'Produccion.Maquinaria',
+        title: 'Maquinaria',
+        type: 'collapse',
+        icon: 'material-outline:account_tree   ',
+        translate: 'Maquinaria',
+        children: [
+          {
+            id: 'Maquinaria.FuncionesMaquina',
+            title: 'Funciones de Máquinas',
+            type: 'item',
+            icon: 'material-outline:bike_scooter',
+            url: '/FuncionesMaquina/Index',
+          },
+          {
+            id: 'Maquinaria.MaquinaHistorial',
+            title: 'Historial de Máquina',
+            type: 'item',
+            icon: 'material-outline:all_inbox',
+            url: '/MaquinaHistorial/Index',
+          },
+          {
+            id: 'Maquinaria.MarcasMaquina',
+            title: 'Marcas de Máquinas',
+            type: 'item',
+            icon: 'material-outline:auto_stories',
+            url: '/Marcas/Index',
+          },
+          {
+            id: 'Maquinaria.Maquinas',
+            title: 'Máquinas',
+            type: 'item',
+            icon: 'material-outline:precision_manufacturing',
+            url: '/Maquinas/Index',
+          },
+
+          {
+            id: 'Maquinaria.ModelosMaquina',
+            title: 'Modelos de Máquinas',
+            type: 'item',
+            icon: 'material-outline:biotech',
+            url: '/ModelosMaquinas/Index',
+          },
+        ],
+      },
+      {
+        id: 'Produccion.Inventario',
+        title: 'Inventario',
+        type: 'collapse',
+        icon: 'material-outline:store',
+        translate: 'Inventario',
+        children: [
+          {
+            id: 'Inventario.Areas',
+            title: 'Áreas',
+            type: 'item',
+            icon: 'material-outline:all_inbox',
+            url: '/Areas/Index',
+          },
+          {
+            id: 'Inventario.Categorias',
+            title: 'Categorías',
+            type: 'item',
+            icon: 'material-outline:category',
+            url: '/Categoria/Index',
+          },
+          {
+            id: 'Inventario.Lotes',
+            title: 'Lotes',
+            type: 'item',
+            icon: 'material-outline:card_membership',
+            url: '/Lotes/Index',
+          },
+          {
+            id: 'Inventario.Materiales',
+            title: 'Materiales',
+            type: 'item',
+            icon: 'material-outline:bakery_dining',
+            url: '/Materiales/Index',
+          },
+
+ 
+          {
+            id: 'Inventario.Subcategorias',
+            title: 'Subcategorías',
+            type: 'item',
+            icon: 'material-outline:apps',
+            url: '/Subcategorias/Index',
+          },
+          {
+            id: 'Inventario.TipoEmbalaje',
+            title: 'Tipo de Embalaje',
+            type: 'item',
+            icon: 'material-outline:integration_instructions',
+            url: '/TipoEmbalaje/Index',
+          },
+        ],
+      },
+      {
+        id: 'produccion.prendas',
+        title: 'Prendas',
+        type: 'collapse',
+        icon: 'material-outline:checkroom',
+        translate: 'Prendas',
+        children: [
+          {
+            id: 'Prendas.Colores',
+            title: 'Colores',
+            type: 'item',
+            icon: 'material-outline:format_color_fill',
+            url: '/Colores/Index',
+          },
+
+          {
+            id: 'Prendas.Estilos',
+            title: 'Estilos',
+            type: 'item',
+            icon: 'material-outline:design_services',
+            url: '/Estilos/Index',
+          },
+          {
+            id: 'Prendas.Modelos',
+            title: 'Modelos',
+            type: 'item',
+            icon: 'material-outline:document_scanner',
+            url: '/Modelos/Index',
+          },
+          {
+            id: 'Prendas.Tallas',
+            title: 'Tallas',
+            type: 'item',
+            icon: 'material-outline:dry_cleaning',
+            url: '/Tallas/Index',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default navigationConfig;
---
+++ SIMEXPRO Produccion/src/app/configs/routesConfig.js
@@ -0,0 +1,271 @@
+/* eslint-disable react/jsx-pascal-case */
+/* eslint-disable camelcase */
+import FuseUtils from '@fuse/utils';
+import FuseLoading from '@fuse/core/FuseLoading';
+import { Navigate } from 'react-router-dom';
+import settingsConfig from 'app/configs/settingsConfig';
+import TallasIndex from '../Prendas/Tallas/Tallas';
+import SignInConfig from '../main/sign-in/SignInConfig';
+import SignUpConfig from '../main/sign-up/SignUpConfig';
+import SignOutConfig from '../main/sign-out/SignOutConfig';
+import dashboardsConfigs from '../main/dashboards/dashboardsConfigs';
+import pagesConfigs from '../main/pages/pagesConfigs';
+import CiudadesIndex from '../Ubicaciones/Ciudades/Ciudades';
+import ProvinciasIndex from '../Ubicaciones/Provincias/Provincias';
+import PaisesIndex from '../Ubicaciones/Paises/Paises';
+import ColoniasIndex from '../Ubicaciones/Colonias/Colonias';
+import CargosIndex from '../Personas/Cargos/Cargos';
+import OficinasIndex from '../Personas/Oficinas/Oficinas';
+import OficiosProfesiones from '../Personas/OficiosProfesiones/OficiosProfesiones';
+import UsuariosIndex from '../Seguridad/Usuarios/Usuarios';
+import RolesIndex from '../Seguridad/Roles/roles';
+import RolesCrear from '../Seguridad/Roles/roles_crear';
+import EmpleadosIndex from '../Personas/Empleados/Empleados';
+import ProveedoresIndex from '../Personas/Proveedores/Proveedores';
+import CategoriaIndex from '../Inventario/Categoria/Categorias';
+import ColoresIndex from '../Prendas/Colores/Colores';
+import InspeccionesIndex from '../Produccion/Inspecciones/InspeccionesEstado';
+import MaquinaHistorialIndex from '../Maquinaria/MaquinaHistorial/MaquinaHistorial';
+import MaterialesIndex from '../Inventario/Materiales/Materiales';
+import AreasIndex from '../Inventario/Areas/Areas';
+import EstilosIndex from '../Prendas/Estilos/Estilos';
+import FuncionesMaquinaIndex from '../Maquinaria/FuncionesMaquina/FuncionesMaquina';
+import LotesIndex from '../Inventario/Lotes/Lotes';
+import SubcategoriaIndex from '../Inventario/Subcategoria/Subcategorias';
+import TipoEmbalajeIndex from '../Inventario/TipoDeEmbalaje/TipoEmbalaje';
+import AldeaIndex from '../Ubicaciones/Aldeas/Aldeas';
+import OrdenProcesosIndex from '../Produccion/OrdenDeProcesos/OrdenDeProcesos';
+import CalendarApp from '../Produccion/Planificación/CalendarApp';
+import MaquinasIndex from '../Maquinaria/Máquinas/Maquinas';
+import FormaDeEnvioIndex from '../Generales/FormasEnvio/FormasEnvio';
+import MonedasIndex from '../Generales/Monedas/monedas';
+import ModuloIndex from '../Produccion/Modulos/Modulos';
+import ProcesosIndex from '../Produccion/Procesos/Procesos';
+import OrdenCompra_Crear from '../Produccion/OrdenCompra/OrdenCompra_Crear';
+import MarcasIndex from '../Maquinaria/MarcasMaquina/MarcasMaquina';
+import ModelosMaquinaIndex from '../Maquinaria/ModelosMaquina/ModelosMaquina';
+import Revision_de_Calidad_Index from '../Produccion/Revisión de Calidad/RevisionCalidad';
+import OrdenPedido_Index from '../Produccion/OrdenPedidos/OrdenPedido_Index';
+import OrdenPedido_Crear from '../Produccion/OrdenPedidos/OrdenPedido_Crear';
+import PedidosProduccion from '../Produccion/PedidosProduccion/PedidosProduccion';
+import PedidosProduccion_Crear from '../Produccion/PedidosProduccion/PedidosProduccion_Crear';
+import OrdenCompraIndex from '../Produccion/OrdenCompra/OrdenCompra';
+const routeConfigs = [
+  ...dashboardsConfigs,
+  ...pagesConfigs,
+  SignOutConfig,
+  SignInConfig,
+  SignUpConfig,
+];
+
+const routes = [
+  ...FuseUtils.generateRoutesFromConfigs(routeConfigs, settingsConfig.defaultAuth),
+  {
+    path: '/',
+    element: <Navigate to="dashboards/analytics" />,
+    auth: settingsConfig.defaultAuth,
+  },
+  {
+    path: 'loading',
+    element: <FuseLoading />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="pages/error/404" />,
+  },
+  {
+    path: 'Ciudades/Index',
+    element: <CiudadesIndex />,
+  },
+  {
+    path: 'OrdenCompra/Crear',
+    element: <OrdenCompra_Crear />,
+  },
+  {
+    path: 'OrdenCompra/Index',
+    element: <OrdenCompraIndex />,
+  },
+
+  {
+    path: 'OrdenPedido/Crear',
+    element: <OrdenPedido_Crear />,
+  },
+  {
+    path: 'RevisionCalidad/Crear',
+    element: <Revision_de_Calidad_Index />,
+  },
+  {
+    path: 'PedidosProduccion/Index',
+    element: <PedidosProduccion />,
+  },
+  {
+    path: 'PedidosProduccion/Crear',
+    element: <PedidosProduccion_Crear />,
+  },
+  {
+    path: 'RevisionCalidad/Index',
+    element: <Revision_de_Calidad_Index />,
+  },
+  {
+    path: 'Provincias/Index',
+    element: <ProvinciasIndex />,
+  },
+  {
+    path: 'Paises/Index',
+    element: <PaisesIndex />,
+  },
+  {
+    path: 'Colonias/Index',
+    element: <ColoniasIndex />,
+  },
+  {
+    path: 'FormaDeEnvio/Index',
+    element: <FormaDeEnvioIndex />,
+  },
+  {
+    path: 'Monedas/Index',
+    element: <MonedasIndex />,
+  },
+  {
+    path: 'Cargos/Index',
+    element: <CargosIndex />,
+  },
+  {
+    path: 'OrdenCompra/Index',
+    element: <OrdenCompraIndex />,
+  },
+  {
+    path: 'Oficinas/Index',
+    element: <OficinasIndex />,
+  },
+  {
+    path: 'OficiosProfesiones/Index',
+    element: <OficiosProfesiones />,
+  },
+  {
+    path: 'Usuarios/Index',
+    element: <UsuariosIndex />,
+  },
+  {
+    path: 'Empleados/Index',
+    element: <EmpleadosIndex />,
+  },
+  {
+    path: 'Proveedores/Index',
+    element: <ProveedoresIndex />,
+  },
+  {
+    path: 'Roles/RolesIndex',
+    element: <RolesIndex />,
+  },
+  {
+    path: 'Roles/RolesCrear',
+    element: <RolesCrear />,
+  },
+  {
+    path: 'Categoria/Index',
+    element: <CategoriaIndex />,
+  },
+  {
+    path: 'Colores/Index',
+    element: <ColoresIndex />,
+  },
+  {
+    path: 'Inspecciones/Index',
+    element: <InspeccionesIndex />,
+  },
+  {
+    path: 'MaquinaHistorial/Index',
+    element: <MaquinaHistorialIndex />,
+  },
+  {
+    path: 'Categoria/Index',
+    element: <CategoriaIndex />,
+  },
+  {
+    path: 'Colores/Index',
+    element: <ColoresIndex />,
+  },
+  {
+    path: 'Inspecciones/Index',
+    element: <InspeccionesIndex />,
+  },
+  {
+    path: 'MaquinaHistorial/Index',
+    element: <MaquinaHistorialIndex />,
+  },
+  {
+    path: 'Materiales/Index',
+    element: <MaterialesIndex />,
+  },
+  {
+    path: 'Areas/Index',
+    element: <AreasIndex />,
+  },
+  {
+    path: 'Estilos/Index',
+    element: <EstilosIndex />,
+  },
+  {
+    path: 'FuncionesMaquina/Index',
+    element: <FuncionesMaquinaIndex />,
+  },
+  {
+    path: 'Lotes/Index',
+    element: <LotesIndex />,
+  },
+  {
+    path: 'Subcategorias/Index',
+    element: <SubcategoriaIndex />,
+  },
+  {
+    path: 'TipoEmbalaje/Index',
+    element: <TipoEmbalajeIndex />,
+  },
+  {
+    path: 'Aldea/Index',
+    element: <AldeaIndex />,
+  },
+  {
+    path: 'OrdenProcesos/Index',
+    element: <OrdenProcesosIndex />,
+  },
+  {
+    path: 'Planificacion/Index',
+    element: <CalendarApp />,
+  },
+  {
+    path: 'Maquinas/Index',
+    element: <MaquinasIndex />,
+  },
+  {
+    path: 'Modulo/Index',
+    element: <ModuloIndex />,
+  },
+  {
+    path: 'Procesos/Index',
+    element: <ProcesosIndex />,
+  },
+  {
+    path: 'Marcas/Index',
+    element: <MarcasIndex />,
+  },
+  {
+    path: 'ModelosMaquinas/Index',
+    element: <ModelosMaquinaIndex />,
+  },
+  {
+    path: 'ModelosMaquinas/Index',
+    element: <ModelosMaquinaIndex />,
+  },
+  {
+    path: 'Tallas/Index',
+    element: <TallasIndex />,
+  },
+  {
+    path: 'OrdenPedido/Index',
+    element: <OrdenPedido_Index />,
+  },
+];
+
+export default routes;
